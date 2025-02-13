f985716c3e316251270c9bdb1c9d62465282495f
Axlxe05 <allaeclash@gmail.com>
2025-02-13 20:17:30 +0100

    servicio 3 finished

--- servicio3/app.py
+++ servicio3/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify
+import requests
 import json
 
 app3 = Flask(__name__)
 
+@app3.route('/<int:municipioid>/meteo', methods=['GET'])
+def get_meteo(municipioid):
+    resp = requests.get("https://www.el-tiempo.net/api/json/v2/provincias/14/municipios/14037")
+    diccionario = resp.json()
+    #diccionario = json.load(data)
+    print(diccionario["municipio"]["CODIGOINE"][0:5])
+    if(diccionario["municipio"]["CODIGOINE"][0:5]==str(municipioid)):
+            return jsonify(diccionario)
+    else:
+            return "{\"info\": \"Meteorologia no disponible\"}"
 
 if __name__ == '__main__':
     app3.run(port=5002)
